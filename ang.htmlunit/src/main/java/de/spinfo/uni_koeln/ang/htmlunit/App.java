f06596c1aac26bd3e4af218e1405bf928471f3a9
fmondaca <f.mondaca@uni-koeln.de>
2015-11-11 19:18:20 +0100

    ang.htmlunit added

---
+++ ang.htmlunit/src/main/java/de/spinfo/uni_koeln/ang/htmlunit/App.java
@@ -0,0 +1,13 @@
+package de.spinfo.uni_koeln.ang.htmlunit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
