44dc6041f1922b30c4e6b6665eaad9abdc99aef0
Peter Seipel <Pseipel@web.de>
2015-11-20 12:15:05 +0100

    App main

--- ang.htmlunit/src/main/java/de/spinfo/uni_koeln/ang/htmlunit/App.java
+++ ang.htmlunit/src/main/java/de/spinfo/uni_koeln/ang/htmlunit/App.java
@@ -1,13 +1,13 @@
 package de.spinfo.uni_koeln.ang.htmlunit;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.IOException;
+
+public class App {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+ 		S1 s1 = new S1();
+		s1.listReader();
+
+	}
+
 }
