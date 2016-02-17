55308f04e165afa8bbb1138ef56ddd903166d0b1
bkiss <boerge.kiss@gmail.com>
2016-02-17 15:54:06 +0100

    IO utility class

---
+++ ang.twitter/src/main/java/de/uni_koeln/spinfo/ang/util/IO.java
@@ -0,0 +1,54 @@
+package de.uni_koeln.spinfo.ang.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+public class IO {
+	
+	public static String readFile(String path){
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			BufferedReader in = getReader(path);
+			String str;
+			      
+			while ((str = in.readLine()) != null) {
+			    sb.append(str);
+			    sb.append("\n");
+			}
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
+	
+	public static BufferedReader getReader(String filePath){
+		try {
+			return new BufferedReader(
+					new InputStreamReader(
+							   new FileInputStream(
+									   new File(filePath)), "UTF8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static boolean deleteFile(String path){
+		File f = new File(path);
+		if (!f.exists() || !f.canWrite() || !f.delete()){
+			System.err.println("[IO]\tcannot delete file: " + path);
+			return false;
+		}
+		return true;
+	}
+
+}
