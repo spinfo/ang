28120b36ea67094fb38a90c53e398667512c8cda
bkiss <boerge.kiss@gmail.com>
2016-02-17 15:52:47 +0100

    patterns class to hold all the needed regex patterns

---
+++ ang.twitter/src/main/java/de/uni_koeln/spinfo/ang/preprocess/Patterns.java
@@ -0,0 +1,15 @@
+package de.uni_koeln.spinfo.ang.preprocess;
+
+public class Patterns {
+	
+	public static final String TWITTER_MENTION 				= "(?<=^|(?<=[^a-zA-Z0-9-\\.]))@([A-Za-z0-9_]+)([^a-zA-Z0-9_]|$)";
+	public static final String TWITTER_HASHTAG 				= "(?<=^|(?<=[^a-zA-Z0-9-_\\.]))#([A-Za-z]+[A-Za-z0-9_]+)";
+	public static final String PATTERN_TWITTER_RETWEET 		= "RT\\s" + TWITTER_MENTION + "\\s";
+	public static final String PATTERN_UNICODES				= "\\\\u(?=[a-fA-F0-9]{4})";
+	public static final String PATTERN_HAS_LATIN_CHARS		= ".*\\p{L}+.*";
+	public static final String PATTERN_URL					= "(http|https|ftp)\\:\\/\\/[^\\s$]+(?=(\\s|$))";
+	public static final String PATTERN_JSON_OBJECT 			= "^\\{.*\\}$";
+	public static final String PATTERN_JSON_OBJECT_LANG_DE 	= "^.*\\\"lang\\\"\\:\\\"de\\\".*$";
+	public static final String PATTERN_WORD_BOUNDARY 		= "\\P{L}";
+
+}
