25579b35a47e1e4be417c023e968b7816d76e038
bkiss <boerge.kiss@gmail.com>
2016-02-23 15:07:14 +0100

    simple FormatConvert string conversion utility class

---
+++ ang.twitter/src/main/java/de/uni_koeln/spinfo/ang/util/FormatConvert.java
@@ -0,0 +1,22 @@
+package de.uni_koeln.spinfo.ang.util;
+
+public class FormatConvert {
+	
+	public static int monthWordShortToNumber(String month){
+		month = month.toUpperCase();
+		if		(month.equals("JAN")) return 1;
+		else if (month.equals("FEB")) return 2;
+		else if (month.equals("MAR")) return 3;
+		else if (month.equals("APR")) return 4;
+		else if (month.equals("MAY")) return 5;
+		else if (month.equals("JUN")) return 6;
+		else if (month.equals("JUL")) return 7;
+		else if (month.equals("AUG")) return 8;
+		else if (month.equals("SEP")) return 9;
+		else if (month.equals("OKT")) return 10;
+		else if (month.equals("NOV")) return 11;
+		else if (month.equals("DEZ")) return 12;
+		else return -1;
+	}
+
+}
