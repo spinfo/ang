8a280efa8791002cee547b152636ffa8e28f7ebe
bkiss <boerge.kiss@gmail.com>
2016-02-23 18:11:20 +0100

    CorpusObject class to represent data and build BSON Documents

---
+++ ang.twitter/src/main/java/de/uni_koeln/spinfo/ang/data/CorpusObject.java
@@ -0,0 +1,32 @@
+package de.uni_koeln.spinfo.ang.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bson.Document;
+
+public class CorpusObject {
+	
+	private Map<String, Object> data;
+	
+	public CorpusObject(){
+		data = new HashMap<String, Object>();
+	}
+	
+	public void addData(CorpusObjectField fieldKey, String data){
+		this.data.put(fieldKey.toString(), data);
+	}
+	
+	public void removeData(CorpusObjectField fieldKey){
+		data.remove(fieldKey.toString());
+	}
+	
+	public String getData(CorpusObjectField fieldKey){
+		return (String)data.get(fieldKey.toString());
+	}
+	
+	public Document getBsonDocument(){
+		return new Document(data);
+	}
+
+}
