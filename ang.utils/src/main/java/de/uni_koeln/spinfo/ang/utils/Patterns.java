2bb35efae0beee88a2e2f44ebd8a18f9eb116589
bkiss <boerge.kiss@gmail.com>
2016-03-02 16:31:12 +0100

    added patterns for inline month and year strings

--- ang.utils/src/main/java/de/uni_koeln/spinfo/ang/utils/Patterns.java
+++ ang.utils/src/main/java/de/uni_koeln/spinfo/ang/utils/Patterns.java
@@ -15,9 +15,11 @@ public class Patterns {
 	public static final String CONTROL_CHARS	 		= "[\\x11-\\x14]";
 	public static final String DATE_YEAR		 		= "(18|19|20)\\d\\d";
 	public static final String DATE_MONTH_WORD_SHORT	= "(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dez)";
+	public static final String DATE_YEAR_INLINE			= "(?<=.*\\d\\d\\.\\d\\d\\.)(18|19|20)\\d\\d(?=.*)";
+	public static final String DATE_MONTH_INLINE		= "(?<=.*\\d\\d\\.)\\d\\d(?=\\.(18|19|20)\\d\\d.*)";
 	public static final String TWITTER_JSON_FILES		= ".+\\.(json|geo)";
 	public static final String EVERYTHING				= ".*";
 	public static final String WACKY_URL_LINE			= "^CURRENT URL.*";
-	
+
 
 }
