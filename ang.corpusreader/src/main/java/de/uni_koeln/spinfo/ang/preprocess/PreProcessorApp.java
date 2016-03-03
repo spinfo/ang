2ecba5c93bc23df950a5fbf41f08b52e98f3c407
bkiss <boerge.kiss@gmail.com>
2016-03-03 14:47:28 +0100

    generic pre-processor app as entry point

---
+++ ang.corpusreader/src/main/java/de/uni_koeln/spinfo/ang/preprocess/PreProcessorApp.java
@@ -0,0 +1,24 @@
+package de.uni_koeln.spinfo.ang.preprocess;
+
+import de.uni_koeln.spinfo.ang.utils.Patterns;
+
+public class PreProcessorApp {
+
+	public static void main(String[] args) {
+		if (args.length < 1){
+			System.err.println("[USAGE]\tthis program expects 1 parameter (path to a directory)");
+			return;
+		}
+		
+		AbstractPreProcessor pp = new DSAStrukturPreProcessor();
+		pp.process(args[0],
+				Patterns.FILE_NAME_ADD_EXTENSION + "txt", //file name pattern
+				"", //db user
+				"", //db pass
+				"ang", //db name
+				"localhost", //db host
+				"27017", //db port
+				"angdata"); //db collection name
+	}
+
+}
