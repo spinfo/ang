af4a2eae03e9daaec8bf6f673181432f6b2b5094
bkiss <boerge.kiss@gmail.com>
2016-03-21 14:04:39 +0100

    spark init test

---
+++ ang.ui/src/main/java/ang/ui/Test.java
@@ -0,0 +1,12 @@
+package ang.ui;
+
+import static spark.Spark.*;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+        //get("/hello", (req, res) -> "Hello World");
+    }
+
+}
