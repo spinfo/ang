36460009b74597aacb709c15b5ca1d0fba722c1b
bkiss <boerge.kiss@gmail.com>
2016-03-21 18:11:54 +0100

    basic Web-UI prototype

--- ang.utils/src/main/java/de/uni_koeln/spinfo/ang/utils/MongoWrapper.java
+++ ang.utils/src/main/java/de/uni_koeln/spinfo/ang/utils/MongoWrapper.java
@@ -1,11 +1,14 @@
 package de.uni_koeln.spinfo.ang.utils;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.bson.Document;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -53,6 +56,21 @@ public class MongoWrapper {
 	public void addDocuments(List<Document> corpusDocuments){
 		if (!isInitiated()) return;
 		coll.insertMany(corpusDocuments);
+	}
+	
+	
+	public FindIterable<Document> getSearchResults(String queryRegex, String source){
+		if (!isInitiated() || queryRegex == null || queryRegex.length() == 0) return null;
+		
+		BasicDBObject q = new BasicDBObject();
+		q.put("text", Pattern.compile(queryRegex, Pattern.CASE_INSENSITIVE));
+		if (source != null) q.put("source", source);
+		
+		BasicDBObject keys = new BasicDBObject();
+		keys.put("text", 1);
+		keys.put("source", 1);
+		
+		return coll.find(q).projection(keys);
 	}
 	
 	
