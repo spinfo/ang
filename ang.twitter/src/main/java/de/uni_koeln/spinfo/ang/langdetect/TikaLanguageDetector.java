a8ed694c2d2a1bacb2826d38524a03cbedb5b89b
bkiss <boerge.kiss@gmail.com>
2016-03-24 11:12:44 +0100

    Tika detector

--- ang.twitter/src/main/java/de/uni_koeln/spinfo/ang/langdetect/TikaLanguageDetector.java
+++ ang.twitter/src/main/java/de/uni_koeln/spinfo/ang/langdetect/TikaLanguageDetector.java
@@ -2,12 +2,17 @@ package de.uni_koeln.spinfo.ang.langdetect;
 
 import org.apache.tika.language.LanguageIdentifier;
 
-public class TikaGermanDetector implements IGermanDetector{
+public class TikaLanguageDetector implements IGermanDetector{
 	
 	@Override
 	public boolean isGerman(String text) {
 		LanguageIdentifier identifier = new LanguageIdentifier(text);
 		return identifier.getLanguage().equals("de");
+	}
+	
+	public boolean isEnglish(String text) {
+		LanguageIdentifier identifier = new LanguageIdentifier(text);
+		return identifier.getLanguage().equals("en");
 	}
 
 }
