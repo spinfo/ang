7038bcfc101c0dfcbbacf5eda536520ea5ebf240
bkiss <boerge.kiss@gmail.com>
2016-04-06 16:03:58 +0200

    cleanup

--- angui.webapp/src/main/java/ang/ui/MongoWrapper.java
+++ angui.webapp/src/main/java/ang/ui/MongoWrapper.java
@@ -1,7 +1,6 @@
 package ang.ui;
 
 import java.util.List;
-import java.util.regex.Pattern;
 
 import org.bson.Document;
 
--- angui.webapp/src/main/java/ang/ui/WebApp.java
+++ angui.webapp/src/main/java/ang/ui/WebApp.java
@@ -9,7 +9,6 @@ import java.io.BufferedInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
